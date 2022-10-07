7eb0667eb9643834a5e044044e41044b54609717
Hannelore Lübeck <honeycoffeeandmilk@gmail.com>
2022-10-07 16:38:57 +0200

    Rotate function implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,8 +26,13 @@ public class Main {
         BufferedImage trackingNumber = bi.getSubimage(970, 230, 160 , bi.getHeight()/2-450);
         BufferedImage barcodes= bi.getSubimage(1485, 230, 850 , bi.getHeight()/2-450);
 
-        File outputfile = new File("/Users/hanni/Programming/Java/test3/image.png");
-        ImageIO.write(SubImg, "png", outputfile);
+
+       // File outputfile = new File("/Users/hanni/Programming/Java/test3/image.png");
+        //ImageIO.write(SubImg, "png", outputfile);
+
+        File outputfile = new File("/Users/hanni/Programming/Java/test3/lable.png");
+        BufferedImage ni = joinLabel(header, securityCode, addresses, trackingNumber, barcodes);
+        ImageIO.write(ni, "png", outputfile);
         // format 62mm x 180mm
 
         System.out.println("Width: " + bi.getWidth());
@@ -37,21 +42,32 @@ public class Main {
     public static BufferedImage joinLabel(BufferedImage header, BufferedImage securityCode, BufferedImage addresses, BufferedImage trackingNumber, BufferedImage barcodes) {
         BufferedImage[] Images  = {header, securityCode, addresses, trackingNumber, barcodes};
         for (int i = 0; i < Images.length; i++) {
-
+            Images[i] = rotate(Images[i], 90);
         }
-//        int offset = 2;
-//        int width = img1.getWidth() + img2.getWidth() + offset;
-//        int height = Math.max(img1.getHeight(), img2.getHeight()) + offset;
-        BufferedImage newImage = new BufferedImage(20, 20, BufferedImage.TYPE_INT_ARGB);
-//        Graphics2D g2 = newImage.createGraphics();
-//        Color oldColor = g2.getColor();
-//        g2.setPaint(Color.WHITE);
-//        g2.fillRect(0, 0, width, height);
-//        g2.setColor(oldColor);
-//        g2.drawImage(img1, null, 0, 0);
+        int width = 2613;
+        int height = 900;
+        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = newImage.createGraphics();
+//       Color oldColor = g2.getColor();
+        g2.setPaint(Color.WHITE);
+        g2.fillRect(0, 0, width, height);
+  //      g2.setColor(oldColor);
+        g2.drawImage(Images[4], null, 0, 0);
 //        g2.drawImage(img2, null, img1.getWidth() + offset, 0);
-//        g2.dispose();
+        g2.dispose();
         return newImage;
+    }
+
+    public static BufferedImage rotate(BufferedImage bimg, double angle) {
+        int w = bimg.getWidth();
+        int h = bimg.getHeight();
+        BufferedImage rotated = new BufferedImage(h, w, bimg.getType());
+        Graphics2D graphic = rotated.createGraphics();
+        graphic.translate(-(w - h) / 2, (h - w) / 2);
+        graphic.rotate(Math.toRadians(angle), h/2, w/2);
+        graphic.drawImage(bimg, null, 0, 0);
+        graphic.dispose();
+        return rotated;
     }
 
     // cut image
